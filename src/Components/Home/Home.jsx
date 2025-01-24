0cf270c5941902c4f79881dbe5cacfc855c4b856
pamilerin17 <pamilerinerinayo@gmail.com>
2025-01-24 17:18:36 +0100

    already done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 import Food1 from '../../assets/breakfast-1.jpg'
 import Food2 from '../../assets/breakfast-2.jpg'
 import Food3 from '../../assets/breakfast-3.jpg'
-import Food4 from '../../assets/breakfast-4.jpg'
+
 
 function Home() {
   return (
@@ -29,11 +29,7 @@ function Home() {
         <h2>Grilled Beef with Potatoes</h2>
         <h1>Meat, Potatoes, Rice, Tomatoes</h1>
        </div>
-             <div className="food">
-        <img src={Food4} alt="" />
-        <h2>Grilled Beef with Potatoes</h2>
-        <h1>Meat, Potatoes, Rice, Tomatoes</h1>
-       </div>
+
       </section>
     </div>
   )
